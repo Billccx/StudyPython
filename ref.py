b48cc74fe5a4b2b0bfda2a420e2a4538c43f0576
Billccx <52842460+Billccx@users.noreply.github.com>
2021-07-29 14:02:54 +0800

    first commit

---
+++ ref.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+# @Project  :StudyPython
+# @File     :ref
+# @Date     :2021/7/29 9:28
+# @Author   :CuiChenxi
+# @Email    :billcuichenxi@163.com
+# @Software :PyCharm
+-------------------------------------------------
+"""
+print(id(258))
+a = 258
+print(id(a))
+b = 258
+print(id(b))
+print(a is b)
